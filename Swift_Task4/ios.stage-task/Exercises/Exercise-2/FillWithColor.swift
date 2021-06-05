678576b35b697f10fd68ab9e982a98d2f7fb6653
Nadezhda Repina <repinanr@yandex.ru>
2021-06-05 21:17:13 +0300

    feat: fill color task solution

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,82 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        guard newColor < 65536 else { return image }
+        var output = image
+        let number = image[row][column]
+        var rowTmpBack = row - 1;
+        var rowTmpNext = row + 1;
+        output[row] = fillWithColorRow(output[row], column, number, newColor)
+            
+        while rowTmpBack >= 0 {
+            output[rowTmpBack] = fillWithColorRow(image[rowTmpBack], column, number, newColor)
+            output[rowTmpBack] = fillWithColorRowWithLastRow(image: image[rowTmpBack], imageLast: image[rowTmpBack + 1], newColor: newColor, number: number)
+            rowTmpBack -= 1
+        }
+            
+        while rowTmpNext < image.count {
+            output[rowTmpNext] = fillWithColorRow(image[rowTmpNext], column, number, newColor)
+            output[rowTmpNext] = fillWithColorRowWithLastRow(image: image[rowTmpNext], imageLast: image[rowTmpNext - 1], newColor: newColor, number: number)
+            rowTmpNext += 1
+        }
+    
+        return output
+    
+    }
+    
+    func fillWithColorRow(_ image: [Int], _ column: Int, _ number: Int, _ newColor: Int) -> [Int] {
+        var output = image
+        var continueNext = true
+
+        output[column] = newColor
+
+        var columnTmpBack = column - 1;
+        var columnTmpNext = column + 1;
+
+        while continueNext {
+            if columnTmpBack < 0 {
+                continueNext = false
+                break
+            }
+            if number == image[columnTmpBack] {
+                output[columnTmpBack] = newColor
+                columnTmpBack -= 1
+            } else {
+                continueNext = false
+            }
+        }
+        
+        continueNext = true
+        
+        while continueNext {
+            if columnTmpNext >= image.count {
+                continueNext = false
+                break
+            }
+            
+            if number == image[columnTmpNext] {
+                output[columnTmpNext] = newColor
+                columnTmpNext += 1
+            } else {
+                continueNext = false
+            }
+        }
+    
+    return output
+    
+    }
+
+    func fillWithColorRowWithLastRow(image: [Int], imageLast: [Int], newColor: Int, number: Int) -> [Int] {
+        var index = 0
+        var output = image
+        while index < imageLast.count {
+            if imageLast[index] == newColor && output[index] == number {
+                output[index] = newColor
+            }
+            index += 1
+        }
+    
+    return output
+    
     }
 }
