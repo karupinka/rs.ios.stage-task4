46b981ab42ee35ea9abae2e604ef4f138d28686b
Nadezhda Repina <repinanr@yandex.ru>
2021-06-05 21:16:50 +0300

    feat: first task solution

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,24 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        let romanNumbers = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        let intrNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        var num: Int = self
+
+        guard self > 0  else {
+            return nil
+        }
+        var result = ""
+        while (num > 0) {
+            for (index, i) in intrNumbers.enumerated() {
+                if (i <= num) {
+                    result += romanNumbers[index]
+                    num -= i
+                    break
+                }
+            }
+            
+        }
+        return result
     }
 }
